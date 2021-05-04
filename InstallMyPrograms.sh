d840ceca0905bf39318c3f3c16610fb67e3f9103
xi72yow <65042627+xi72yow@users.noreply.github.com>
2021-05-04 14:54:32 +0200

    Update InstallMyPrograms.sh

--- InstallMyPrograms.sh
+++ InstallMyPrograms.sh
@@ -79,29 +79,38 @@ gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 
 #Dash-To-Panel install
 
-#-- Create Temporary Folder ------------------------------------
+#Create Temporary Folder 
 declare folder_name="$RANDOM-dash-to-panel";
 declare folder_path="/tmp/$folder_name"
 
 mkdir "$folder_path";
 cd "$folder_path";
 
-#-- Download Source Code ------------------------------------
+#Download Source Code 
 sudo apt install git -y;
 git clone "https://github.com/home-sweet-gnome/dash-to-panel.git";
 cd "dash-to-panel/";
 
-#-- Install Dependencies  ------------------------------------
+#Install Dependencies
 sudo apt install gettext -y;
 sudo apt install make -y;
 
-#-- Install Extension ------------------------------------
+#Install Extension
 make install;
 gnome-extensions enable "dash-to-panel@jderose9.github.com";
+
+#Remove Temporary Folder
+rm -rf "$folder_path";
+
+#Display Config
+curl --location --output DisplayConfig.zip --write-out "%{url_effective}\n" "https://github.com/xi72yow/DisplayConfig/archive/refs/heads/main.zip"
+unzip -q DisplayConfig.zip -d ~/.local/share/gnome-shell/extensions/
+gnome-extensions enable "DisplayConfig";
+
+#restart Gnome Shell
 busctl --user call "org.gnome.Shell" "/org/gnome/Shell" "org.gnome.Shell" "Eval" "s" 'Meta.restart("Restarting…")';
 
-#-- Remove Temporary Folder ------------------------------------
-rm -rf "$folder_path";
+#---not so importent stuff---#
 
 #VM Example: sudo kvm -m 5G -hdb /dev/sdb
 sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst virt-manager -y
