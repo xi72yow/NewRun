7056ece796601737aa1e55fed16a89962922c631
xi72yow <65042627+xi72yow@users.noreply.github.com>
2021-05-04 14:26:25 +0200

    Update InstallMyPrograms.sh

--- InstallMyPrograms.sh
+++ InstallMyPrograms.sh
@@ -77,6 +77,32 @@ gsettings set org.gnome.desktop.background picture-uri "file://$PWD/XSmileWhite.
 gsettings set org.gnome.desktop.interface clock-format "24h"
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 
+#Dash-To-Panel install
+
+#-- Create Temporary Folder ------------------------------------
+declare folder_name="$RANDOM-dash-to-panel";
+declare folder_path="/tmp/$folder_name"
+
+mkdir "$folder_path";
+cd "$folder_path";
+
+#-- Download Source Code ------------------------------------
+sudo apt install git -y;
+git clone "https://github.com/home-sweet-gnome/dash-to-panel.git";
+cd "dash-to-panel/";
+
+#-- Install Dependencies  ------------------------------------
+sudo apt install gettext -y;
+sudo apt install make -y;
+
+#-- Install Extension ------------------------------------
+make install;
+gnome-extensions enable "dash-to-panel@jderose9.github.com";
+busctl --user call "org.gnome.Shell" "/org/gnome/Shell" "org.gnome.Shell" "Eval" "s" 'Meta.restart("Restarting…")';
+
+#-- Remove Temporary Folder ------------------------------------
+rm -rf "$folder_path";
+
 #VM Example: sudo kvm -m 5G -hdb /dev/sdb
 sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst virt-manager -y
 
