98e4b690e14e0e93570e65605b77b5a3ce686633
xi72yow <65042627+xi72yow@users.noreply.github.com>
2021-12-27 22:48:02 +0100

    Update InstallMyPrograms.sh

--- InstallMyPrograms.sh
+++ InstallMyPrograms.sh
@@ -59,10 +59,9 @@ sudo apt install ckb-next
 sudo apt install alsa alsa-tools sox mplayer kid3 -y
 #ffmpeg Example: Audio: ffmpeg -i input.wav -vn -ar 44100 -ac 2 -b:a 320k output.mp3 Video: ffmpeg -i myvideo.mp4 -b:a 320k output.mp3
 sudo apt install ffmpeg -y
-#freac CD Ripper
 
 #Multimedia
-sudo apt install openshot-qt blender gparted ktorrent lmms flameshot mumble birdfont filezilla obs-studio inkscape pdfmod -y
+sudo apt install openshot-qt blender gparted ktorrent lmms flameshot mumble birdfont filezilla obs-studio inkscape pdfmod handbrake libfdk-aac1 -y
 
 #Insync Install Not sure that is in every case the newest Version
 curl https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.3.6.40933-focal_amd64.deb --output Incync_amd64.deb
@@ -88,30 +87,10 @@ gsettings set org.gnome.desktop.background picture-uri "file://$PWD/XSmileWhite.
 gsettings set org.gnome.desktop.interface clock-format "24h"
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 
-#Dash-To-Panel install
-
-#Create Temporary Folder 
-declare folder_name="$RANDOM-dash-to-panel";
-declare folder_path="/tmp/$folder_name"
-
-mkdir "$folder_path";
-cd "$folder_path";
-
-#Download Source Code 
-sudo apt install git -y;
-git clone "https://github.com/home-sweet-gnome/dash-to-panel.git";
-cd "dash-to-panel/";
-
-#Install Dependencies
-sudo apt install gettext -y;
-sudo apt install make -y;
-
-#Install Extension
-make install;
-gnome-extensions enable "dash-to-panel@jderose9.github.com";
-
-#Remove Temporary Folder
-rm -rf "$folder_path";
+#Keymapper
+download_URL=$(get_URL_from_latest_release_for_deb "sezanzeb/key-mapper")
+curl --location --output key-mapper.deb --write-out "%{url_effective}\n" $download_URL
+sudo apt install ./key-mapper.deb -y
 
 #Display Config
 curl --location --output DisplayConfig.zip --write-out "%{url_effective}\n" "https://github.com/xi72yow/DisplayConfig/archive/refs/heads/main.zip"
@@ -120,34 +99,3 @@ gnome-extensions enable "DisplayConfig";
 
 #restart Gnome Shell
 busctl --user call "org.gnome.Shell" "/org/gnome/Shell" "org.gnome.Shell" "Eval" "s" 'Meta.restart("Restarting…")';
-
-#---not so importent stuff---#
-
-#VM Example: sudo kvm -m 5G -hdb /dev/sdb
-sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst virt-manager -y
-
-#Microrechnerentwurf
-sudo apt install gcc git wget make libncurses-dev flex bison gperf python3 python3-serial -y
-
-##ESP Toolchain
-mkdir -p ~/esp
-cd ~/esp
-tar -xzf ~/Downloads/xtensa-lx106-elf-linux64-1.22.0-100-ge567ec7-5.2.0.tar.gz
-
-sudo echo "alias get_lx106='export PATH="$PATH:$HOME/esp/xtensa-lx106-elf/bin"'" >> ~/.bash_aliases
-
-sudo usermod -a -G dialout $USER
-sudo chmod -R 777 /dev/ttyUSB0
-
-#ESP8266_RTOS_SDK
-git clone --recursive https://github.com/espressif/ESP8266_RTOS_SDK.git
-pip3 install -r ~/requirements.txt
-
-#Meshlab
-
-#Insync do not work LEL
-##apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ACCAF35C
-##cd /etc/apt/sources.list.d/
-##printf 'deb http://apt.insync.io/ubuntu Focal Fossa non-free contrib' >insync.list #maybe FocalFosse together??
-##sudo apt-get update
-##sudo apt-get install insync
